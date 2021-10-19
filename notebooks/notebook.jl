bcad350f71666adfdc734cd7fad903b3b2b2b08b
Noah Gardner <ngngardner@gmail.com>
2021-10-19 19:51:13 -0400

    dataset progress

--- notebooks/notebook.jl
+++ notebooks/notebook.jl
@@ -46,8 +46,16 @@ function extract_def(path)
 		pos = arr[3]
 		parser = arr[4]
 		def = join(arr[5:end])
+			
 		
-		if lang != "eng"
+		
+		# ignore proper nouns
+		if pos == "Proper noun"
+			continue
+		end
+		
+		# ignore self referential definitions
+		if occursin(word, def)
 			continue
 		end
 		
@@ -59,6 +67,7 @@ function extract_def(path)
 		
 		append!(res, row)
 	end
+	
 	return res
 end
 
@@ -83,17 +92,48 @@ end
 # ╔═╡ d4311972-a821-40be-982a-fb915d24ffce
 etym_clean = clean_etym(etym_df)
 
-# ╔═╡ 8830fb0d-9edf-4ff2-ab2a-3fd16186b693
-function col_join(strings)
-	# use join with space delim
-	return join(strings, " ")
+# ╔═╡ 2e5c3c1f-2192-4301-b5c7-51c4462ee80e
+begin
+	function max_entries(x, num)
+		# limit number of entries in x to num
+		if size(x)[1] > num
+			return x[1:num]
+		end
+		return x
+	end
+	
+	function remove_context(x)
+		# remove definition contexts that are wrapped in double curly brace
+		return replace(x, r"{{.*}}\s"=>"")
+	end
+	
+	function clean_def_group(x)
+		# given a group of definitions, apply the above functions
+		x = remove_context(x)
+		
+		# limit to three definitions per word
+		x = max_entries(x, 3)
+		
+		return x
+	end
 end
 
 # ╔═╡ d28976f1-c1e3-4f1a-a2c5-4eb81fe9875d
 function clean_def(df)
-	fdf = filter(:pos => !=("Proper noun"), df)
-	gdf = groupby(fdf, [:word, :pos])
-	return combine(gdf, :def => col_join => :def)
+	# keep english sources
+	edf = filter(:lang => ==("eng"), df)
+	
+	# remove proper pronouns
+	pdf = filter(:pos => !=("Proper noun"), edf)
+	
+	# remove self referential definitions
+	
+	
+	# remove definition context
+	
+	
+	gdf = groupby(edf, [:word])
+	return combine(gdf, :def => (x -> clean_def_group(x)) => :def)
 end
 
 # ╔═╡ dfbe48df-e71e-45f2-9f56-7f0e37a0fdc7
@@ -119,7 +159,7 @@ TODO:
 # ╠═e9ddaa82-11e1-4bc2-98cd-ec01c881f343
 # ╠═7b8e6d2f-1e65-4647-93ac-649e765455b5
 # ╠═d4311972-a821-40be-982a-fb915d24ffce
-# ╠═8830fb0d-9edf-4ff2-ab2a-3fd16186b693
+# ╠═2e5c3c1f-2192-4301-b5c7-51c4462ee80e
 # ╠═d28976f1-c1e3-4f1a-a2c5-4eb81fe9875d
 # ╠═dfbe48df-e71e-45f2-9f56-7f0e37a0fdc7
 # ╠═4a3f6b0f-2cb7-4fe1-a5f8-5b48c01101a8

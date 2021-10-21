19d16249ff3e20888208623de56313485dc2d5a0
ngngardner <ngngardner@gmail.com>
2021-10-21 14:07:06 -0700

    move julia, add arrow

--- experio/experio.jl
+++ experio/experio.jl
@@ -1,6 +1,164 @@
 
-using Yawipa
-
 function imported()
-    # println("Hello World")
+    println("Hello from Julia!")
 end
+
+import Pkg
+Pkg.activate("../.")
+
+using Arrow
+using CSV
+using DataFrames
+using Statistics
+
+BASE_PATH = "../data/"
+
+"""
+    Loads a CSV file into a DataFrame.
+
+    :param path: The path to the CSV file.
+    :param name: The name of the DataFrame.
+    :param builder: The builder to use.
+    :param from_txt: Whether to read the file as a text file.
+    :param rebuild: Whether to rebuild the DataFrame.
+    :return: The DataFrame.
+"""
+function load_csv(path, name, builder; from_txt=true, rebuild=false)
+	csv_path = string(path, name, ".csv")
+	
+	if !isfile(csv_path) || rebuild
+		if from_txt
+			txt_path = string(path, name, ".txt")
+			df = builder(txt_path)
+		else
+			df = builder()
+		end
+		
+		CSV.write(csv_path, df)
+		return dropmissing(df)
+	end
+		
+	res = CSV.read(csv_path, DataFrame;types=String)
+	return dropmissing(res)
+end
+
+function load_arrow(path, name, builder; from_txt=true, rebuild=false)
+    arrow_path = string(path, name, ".arrow")
+
+    if !isfile(arrow_path)
+        df = load_csv(path, name, builder; from_txt=from_txt, rebuild=rebuild)
+        Arrow.write(arrow_path, df)
+        return df
+    end
+
+    df = DataFrame(Arrow.Table(arrow_path))
+    return df
+end
+
+function load_def(;path=BASE_PATH)
+	function build_def(path)
+		res = DataFrame()
+		for line in eachline(path)
+			arr = split(line, '\t')
+
+			lang = arr[1]
+			word = arr[2]
+			pos = arr[3]
+			parser = arr[4]
+			def = join(arr[5:end])
+
+			row = DataFrame(
+				lang=lang,
+				word=word,
+				pos=pos,
+				def=def,
+			)
+
+			append!(res, row)
+		end
+		return res
+	end
+	return load_arrow(path, "def", build_def)
+end
+
+function load_etym(;path=BASE_PATH)
+	function build_etym(path)
+		res = DataFrame()
+		for line in eachline(path)
+			arr = split(line, '\t')
+
+			lang = arr[1]
+			word = arr[2]
+			etym = string("[(", join(arr[5:end], ")("), ")]")
+
+			row = DataFrame(
+				lang=lang,
+				word=word,
+				etym=etym,
+			)
+
+			append!(res, row)
+		end
+		return res
+	end
+	return load_arrow(path, "etym", build_etym)
+end
+
+function clean_etym(df)
+	fdf = filter(:lang => ==("eng"), df)
+	gdf = groupby(fdf, [:word])
+	return combine(gdf, :etym => first => :etym)
+end
+
+
+function clean_def(df)
+	function clean_def_group(x)
+		function max_entries(x, num)
+			# limit number of entries in x to num
+			if size(x)[1] > num
+				return x[1:num]
+			end
+			return x
+		end
+		
+		# limit to three definitions per word
+		x = max_entries(x, 3)
+		
+		return x
+	end
+	
+	# filter non-english
+	fdf = filter(:lang => ==("eng"), df)
+	
+	# remove proper pronouns
+	pdf = filter(:pos => !=("Proper noun"), fdf)
+	
+	# remove definition contexts
+	reg = r"{{.*}}\s"
+	rdf = transform(pdf, :def => ByRow(x -> replace(x, reg=>"")) => :def)
+	
+	# apply clean_def_group function to each word group
+	gdf = groupby(rdf, [:word])
+	return combine(gdf, :def => (x -> clean_def_group(x)) => :def)
+end
+
+function load_dataset(;path=BASE_PATH)
+	function build_dataset()
+        println("Loading Etymology Dataset...")
+		edf = load_etym()
+        println("Loading Definition Dataset...")
+		ddf = load_def()
+
+        println("Cleaning Etymology Dataset...")
+		cedf = clean_etym(edf)
+        println("Cleaning Definition Dataset...")
+		cddf = clean_def(ddf)
+
+        println("Combining Datasets...")
+		return innerjoin(cedf, cddf, on=:word)
+	end
+	return load_arrow(path, "final", build_dataset; from_txt=false)
+end
+
+# EXPORTS
+export(load_dataset)
--- notebooks/notebook.jl
+++ notebooks/notebook.jl
@@ -4,143 +4,11 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 4df79bd4-d5db-4bcf-8728-2fb3bbd9a657
-begin
-	import Pkg
-	Pkg.activate("../.")
-	using DataFrames
-	using CSV
-	using Statistics
-end
-
-# ╔═╡ 11b67007-f893-47e9-b2b7-28f8e7e0a865
-BASE_PATH = "../data/"
-
-# ╔═╡ f4fdf007-5662-4310-9d34-3f47278148a9
-function load_csv(path, name, builder;from_txt=true, rebuild=false)
-	csv_path = string(path, name, ".csv")
-	
-	if !isfile(csv_path) || rebuild
-		if from_txt
-			txt_path = string(path, name, ".txt")
-			df = builder(txt_path)
-		else
-			df = builder()
-		end
-		
-		CSV.write(csv_path, df)
-		return dropmissing(df)
-	end
-		
-	res = CSV.read(csv_path, DataFrame;types=String)
-	return dropmissing(res)
-end
-
-# ╔═╡ d2a6afb9-7887-4617-aca8-00cbfe4e15a9
-function load_def(;path=BASE_PATH)
-	function build_def(path)
-		res = DataFrame()
-		for line in eachline(path)
-			arr = split(line, '\t')
-
-			lang = arr[1]
-			word = arr[2]
-			pos = arr[3]
-			parser = arr[4]
-			def = join(arr[5:end])
-
-			row = DataFrame(
-				lang=lang,
-				word=word,
-				pos=pos,
-				def=def,
-			)
-
-			append!(res, row)
-		end
-		return res
-	end
-	return load_csv(path, "def", build_def)
-end
-
-# ╔═╡ 2f89a1c4-2c68-4251-8436-b37ef1fa2ed2
-function load_etym(;path=BASE_PATH)
-	function build_etym(path)
-		res = DataFrame()
-		for line in eachline(path)
-			arr = split(line, '\t')
-
-			lang = arr[1]
-			word = arr[2]
-			etym = string("[(", join(arr[5:end], ")("), ")]")
-
-			row = DataFrame(
-				lang=lang,
-				word=word,
-				etym=etym,
-			)
-
-			append!(res, row)
-		end
-		return res
-	end
-	return load_csv(path, "etym", build_etym)
-end
-
-# ╔═╡ 7b8e6d2f-1e65-4647-93ac-649e765455b5
-function clean_etym(df)
-	fdf = filter(:lang => ==("eng"), df)
-	gdf = groupby(fdf, [:word])
-	return combine(gdf, :etym => first => :etym)
-end
-
-# ╔═╡ d28976f1-c1e3-4f1a-a2c5-4eb81fe9875d
-function clean_def(df)
-	function clean_def_group(x)
-		function max_entries(x, num)
-			# limit number of entries in x to num
-			if size(x)[1] > num
-				return x[1:num]
-			end
-			return x
-		end
-		
-		# limit to three definitions per word
-		x = max_entries(x, 3)
-		
-		return x
-	end
-	
-	# filter non-english
-	fdf = filter(:lang => ==("eng"), df)
-	
-	# remove proper pronouns
-	pdf = filter(:pos => !=("Proper noun"), fdf)
-	
-	# remove definition contexts
-	reg = r"{{.*}}\s"
-	rdf = transform(pdf, :def => ByRow(x -> replace(x, reg=>"")) => :def)
-	
-	# apply clean_def_group function to each word group
-	gdf = groupby(rdf, [:word])
-	return combine(gdf, :def => (x -> clean_def_group(x)) => :def)
-end
-
-# ╔═╡ 99908e74-1516-450f-9977-309b9b2fc938
-function load_dataset(name;path=BASE_PATH)
-	function build_dataset()
-		edf = load_etym()
-		ddf = load_def()
-
-		cedf = clean_etym(edf)
-		cddf = clean_def(ddf)
-		return innerjoin(cedf, cddf, on=:word)
-	end
-	return load_csv(path, "final", build_dataset; from_txt=false)
-end
+# ╔═╡ 9ee29e42-20f2-4678-96ad-3ffaaa8fe1af
+include("../experio/experio.jl")
 
 # ╔═╡ 0737d9fd-3c0e-4aa5-a474-38b7bfac2045
-final = load_dataset("final")
+final = load_dataset()
 
 # ╔═╡ b87aef52-3a0c-4f35-9b9f-75f201a8aeaf
 # number of definitions
@@ -169,14 +37,7 @@ begin
 end
 
 # ╔═╡ Cell order:
-# ╠═4df79bd4-d5db-4bcf-8728-2fb3bbd9a657
-# ╠═11b67007-f893-47e9-b2b7-28f8e7e0a865
-# ╠═f4fdf007-5662-4310-9d34-3f47278148a9
-# ╠═d2a6afb9-7887-4617-aca8-00cbfe4e15a9
-# ╠═2f89a1c4-2c68-4251-8436-b37ef1fa2ed2
-# ╠═7b8e6d2f-1e65-4647-93ac-649e765455b5
-# ╠═d28976f1-c1e3-4f1a-a2c5-4eb81fe9875d
-# ╠═99908e74-1516-450f-9977-309b9b2fc938
+# ╠═9ee29e42-20f2-4678-96ad-3ffaaa8fe1af
 # ╠═0737d9fd-3c0e-4aa5-a474-38b7bfac2045
 # ╠═b87aef52-3a0c-4f35-9b9f-75f201a8aeaf
 # ╠═5bb3bf23-e382-4d8c-b369-8441accb3793
